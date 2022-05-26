b71ca5e858eecb4f594a79bbd23eadc2aa459aa3
Mehdi <mehditeymorian322@gmail.com>
2022-05-26 20:05:19 +0430

    feat: add user creation

---
+++ internal/mysql/operation/user.go
@@ -0,0 +1,36 @@
+package operation
+
+import (
+	"errors"
+	"github.com/Idea-Thrive/backend/internal/model"
+	"time"
+)
+
+var errNotInsertedInUserTable = errors.New("not inserted in user table")
+
+func (u *Operation) Create(user model.User) (err error) {
+	queryString := "INSERT INTO User (first_name, last_name, email, phone_number, photo_url, personnel_id, gender, role, created_at, updated_at VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?))"
+
+	result, err := u.DB.Exec(queryString,
+		user.FirstName,
+		user.LastName,
+		user.Email,
+		user.PhoneNumber,
+		user.PhotoURL,
+		user.PersonnelID,
+		user.Gender,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	lid, _ := result.LastInsertId()
+	if lid == 0 {
+		return errNotInsertedInUserTable
+	}
+
+	return err
+}
