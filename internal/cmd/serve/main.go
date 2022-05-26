dd6cb5d9dc3337ef6b0aaadfdd10f15cae04cbc8
Amir Hallaji <a.hallaji.b@gmail.com>
2022-05-26 22:26:55 +0430

    correct linter errors for cmd/serve/main

--- internal/cmd/serve/main.go
+++ internal/cmd/serve/main.go
@@ -1,16 +1,18 @@
 package serve
 
 import (
+	"log"
+	"strconv"
+
 	"github.com/Idea-Thrive/backend/internal/config"
 	"github.com/Idea-Thrive/backend/internal/http/handler"
 	"github.com/Idea-Thrive/backend/internal/logger"
 	"github.com/Idea-Thrive/backend/internal/store"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cobra"
-	"log"
-	"strconv"
 )
 
+// Command function.
 func Command() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "serve",
@@ -22,19 +24,20 @@ func Command() *cobra.Command {
 	return cmd
 }
 
+// main function.
 func main(cmd *cobra.Command, args []string) {
 	cfg := config.Load("config.yaml")
 
 	logger := logger.New(cfg.Log)
 
-	s := store.NewStore(nil)
+	str := store.NewStore(nil)
 
 	app := fiber.New()
 
 	handler.Authentication{
 		Secret: cfg.Http.Secret,
 		Logger: logger,
-		Store:  s,
+		Store:  str,
 	}.Register(app.Group("/auth"))
 
 	log.Fatal(app.Listen(":" + strconv.Itoa(cfg.Http.Port)))
