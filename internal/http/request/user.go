c100cfddfd656c203bcc5c192c9089bff9c4f305
Amir Hallaji <a.hallaji.b@gmail.com>
2022-05-26 16:26:56 +0430

    user request sample added.

--- internal/http/request/user.go
+++ internal/http/request/user.go
@@ -1,1 +1,12 @@
 package request
+
+type UserCreation struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	PhotoURL    string `json:"photo_url"`
+	PersonnelID string `json:"personnel_id"`
+	Gender      string `json:"gender"`
+	Role        string `json:"role"`
+}
