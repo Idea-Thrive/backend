8bf20649bd32efb7479f723baf6ee7de7a8798ea
Mehdi <mehditeymorian322@gmail.com>
2022-05-26 20:18:12 +0430

    feat: complete user creation

--- internal/http/handler/user.go
+++ internal/http/handler/user.go
@@ -1,1 +1,51 @@
 package handler
+
+import (
+	"github.com/Idea-Thrive/backend/internal/http/request"
+	"github.com/Idea-Thrive/backend/internal/model"
+	"github.com/Idea-Thrive/backend/internal/store"
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+type User struct {
+	Store  *store.Store
+	Logger *zap.Logger
+}
+
+func (u User) Register(group fiber.Router) {
+	group.Post("/users", u.create)
+}
+
+func (u User) create(c *fiber.Ctx) error {
+	req := new(request.UserCreation)
+
+	if err := c.BodyParser(req); err != nil {
+		u.Logger.Error("failed to parse request body", zap.Error(err))
+
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	user := model.User{
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+		PhotoURL:    req.PhotoURL,
+		PersonnelID: req.PersonnelID,
+		Gender:      req.Gender,
+		Role:        req.Role,
+	}
+
+	if err := u.Store.UserCreate(user); err != nil {
+		u.Logger.Error("failed to create user", zap.Error(err))
+
+		return c.Status(fiber.StatusExpectationFailed).JSON(req)
+	}
+
+	u.Logger.Info("create user successfully")
+
+	return c.Status(fiber.StatusOK).JSON(req)
+}
