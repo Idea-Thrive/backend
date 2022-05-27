41f294380813fa7e7ecc5e7ff7366b2c28da276a
Amir Hallaji <a.hallaji.b@gmail.com>
2022-05-27 14:00:50 +0430

    feat: add mysql dependencies.

--- internal/store/operation/user.go
+++ internal/store/operation/user.go
@@ -8,13 +8,31 @@ import (
 	"github.com/Idea-Thrive/backend/internal/model"
 )
 
-// errNotInsertedInUserTable error.
-var errNotInsertedInUserTable = errors.New("not inserted in user table")
+var (
+	// errNotInsertedInUserTable error.
+	errNotInsertedInUserTable = errors.New("not inserted in user table")
+	// errUserAlreadyExistsInTable error.
+	errUserAlreadyExistsInTable = errors.New("user already exists in user table")
+	// errRetrieveDataFromUserTable error.
+	errRetrieveDataFromUserTable = errors.New("cannot retrieve data from user table")
+)
 
 // UserCreate function.
 func (u *Operation) UserCreate(user model.User) (err error) {
-	queryString := "INSERT INTO User (first_name, last_name, email, phone_number, photo_url, personnel_id," +
-		" gender, role, created_at, updated_at VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?))"
+
+	currEmail := ""
+	errRetrieve := u.DB.QueryRow("SELECT email from User WHERE email = ?", user.Email).Scan(&currEmail)
+
+	if errRetrieve != nil {
+		err = errRetrieveDataFromUserTable
+	}
+
+	if currEmail == user.Email {
+		err = errUserAlreadyExistsInTable
+	}
+
+	queryString := "INSERT INTO User (first_name, last_name, email, phone_number, photo_url, company_id, personnel_id," +
+		" gender, role, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 	result, err := u.DB.Exec(queryString,
 		user.FirstName,
@@ -22,8 +40,10 @@ func (u *Operation) UserCreate(user model.User) (err error) {
 		user.Email,
 		user.PhoneNumber,
 		user.PhotoURL,
+		user.CompanyID,
 		user.PersonnelID,
 		user.Gender,
+		user.Role,
 		time.Now(),
 		time.Now(),
 	)
