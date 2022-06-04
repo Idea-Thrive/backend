f982f1b7847986ef700b0b33834fe1ea8882754b
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-04 17:01:08 +0430

    feat: add idea base store operations

---
+++ internal/store/operation/idea.go
@@ -0,0 +1,48 @@
+package operation
+
+import (
+	"time"
+
+	"github.com/Idea-Thrive/backend/internal/model"
+)
+
+func (u *Operation) IdeaCreate(idea model.Idea) error {
+	return nil
+}
+
+func (u *Operation) IdeaGet(id string) (*model.Idea, error) {
+	return &model.Idea{
+		Title:         "good idea",
+		Category:      "others",
+		Description:   "this is a very good idea",
+		UpVoteCount:   10,
+		DownVoteCount: -5,
+		CreatorID:     "hassan",
+		CompanyID:     "good-company",
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	}, nil
+}
+
+func (u *Operation) IdeaGetAll(size, offset int) ([]model.Idea, error) {
+	idea := model.Idea{
+		Title:         "good idea",
+		Category:      "others",
+		Description:   "this is a very good idea",
+		UpVoteCount:   10,
+		DownVoteCount: -5,
+		CreatorID:     "hassan",
+		CompanyID:     "good-company",
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	}
+
+	ideas := make([]model.Idea, 0)
+	ideas = append(ideas, idea, idea, idea)
+
+	return ideas, nil
+}
+
+func (u *Operation) IdeaDelete(id string) error {
+	return nil
+}
