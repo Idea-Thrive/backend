e359120eaa2c3e8e5a57c27cc731ff8ee0ab7cda
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-05 10:39:56 +0430

    feat: add idea get all handler

--- internal/http/handler/idea.go
+++ internal/http/handler/idea.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"strconv"
+
 	"github.com/Idea-Thrive/backend/internal/model"
 	"github.com/Idea-Thrive/backend/internal/store"
 	"github.com/gofiber/fiber/v2"
@@ -17,6 +19,7 @@ type Idea struct {
 func (i Idea) Register(group fiber.Router) {
 	group.Post("/", i.Create)
 	group.Get("/:id", i.Get)
+	group.Get("/", i.GetAll)
 }
 
 func (i Idea) Create(ctx *fiber.Ctx) error {
@@ -53,5 +56,21 @@ func (i Idea) Get(ctx *fiber.Ctx) error {
 		}) //nolint:wrapcheck
 	}
 
-	return ctx.Status(fiber.StatusOK).JSON(idea)
+	return ctx.Status(fiber.StatusOK).JSON(idea) //nolint:wrapcheck
+}
+
+func (i Idea) GetAll(ctx *fiber.Ctx) error {
+	size, _ := strconv.Atoi(ctx.Query("size", "100"))
+	offset, _ := strconv.Atoi(ctx.Query("offset", "0"))
+
+	ideas, err := i.Store.IdeaGetAll(size, offset)
+	if err != nil {
+		i.Logger.Error("failed to get ideas", zap.Error(err))
+
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to get ideas",
+		}) //nolint:wrapcheck
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(ideas) //nolint:wrapcheck
 }
