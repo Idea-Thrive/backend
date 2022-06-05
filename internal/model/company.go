c53f38bcb8c50b4c01ee97fd915643af90cc7f5f
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-05 18:11:31 +0430

    feat: add company id to company model

--- internal/model/company.go
+++ internal/model/company.go
@@ -6,6 +6,7 @@ import "time"
 type Company struct {
 	Name            string    `json:"name,omitempty"`
 	LogoURL         string    `json:"logo_url,omitempty"`
+	CompanyID       string    `json:"company_id,omitempty"`
 	OwnerNationalID string    `json:"owner_national_id,omitempty"`
 	OwnerFirstName  string    `json:"owner_first_name,omitempty"`
 	OwnerLastName   string    `json:"owner_last_name,omitempty"`
