09694e46ce2771d9876955072028c4fbc891f1bb
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-05 16:54:27 +0430

    feat: add company store operations

--- internal/store/operation/operation.go
+++ internal/store/operation/operation.go
@@ -2,7 +2,9 @@ package operation
 
 import (
 	"database/sql"
+	"time"
 
+	"github.com/Idea-Thrive/backend/internal/model"
 	"go.uber.org/zap"
 )
 
@@ -15,4 +17,24 @@ type Operation struct {
 // NewOperation function.
 func NewOperation(db *sql.DB, logger *zap.Logger) *Operation {
 	return &Operation{DB: db, Logger: logger}
+}
+
+func (u *Operation) CompanyCreate(company model.Company) error {
+	return nil
+}
+
+func (u *Operation) CompanyGet(id string) (*model.Company, error) {
+	return &model.Company{
+		Name:            "test company",
+		LogoURL:         "url",
+		OwnerNationalID: "2520202020202",
+		OwnerFirstName:  "test-first",
+		OwnerLastName:   "test-last",
+		CreatedAt:       time.Now(),
+		UpdatedAt:       time.Now(),
+	}, nil
+}
+
+func (u *Operation) CompanyDelete(id string) error {
+	return nil
 }
