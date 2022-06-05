5bc6e11a6867efa41a134ffaadef608e3b6276ae
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-05 18:20:08 +0430

    change order of the company model

--- internal/model/company.go
+++ internal/model/company.go
@@ -4,9 +4,9 @@ import "time"
 
 // Company struct.
 type Company struct {
+	CompanyID       string    `json:"company_id,omitempty"`
 	Name            string    `json:"name,omitempty"`
 	LogoURL         string    `json:"logo_url,omitempty"`
-	CompanyID       string    `json:"company_id,omitempty"`
 	OwnerNationalID string    `json:"owner_national_id,omitempty"`
 	OwnerFirstName  string    `json:"owner_first_name,omitempty"`
 	OwnerLastName   string    `json:"owner_last_name,omitempty"`
