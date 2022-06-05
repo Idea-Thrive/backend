1b5a399f39920cc077aae5b10c5c71ae68ff7838
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-05 17:36:42 +0430

    refactor: move all handler registration to http package

---
+++ internal/http/app.go
@@ -0,0 +1,51 @@
+package http
+
+import (
+	"github.com/Idea-Thrive/backend/internal/http/handler"
+	"github.com/Idea-Thrive/backend/internal/http/middleware"
+	"github.com/Idea-Thrive/backend/internal/jwt"
+	"github.com/Idea-Thrive/backend/internal/store"
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+type App struct {
+	JWT    *jwt.JWT
+	Store  *store.Store
+	Logger *zap.Logger
+}
+
+func (a App) Register(app *fiber.App) {
+	auth := middleware.Auth{
+		JWT:    a.JWT,
+		Logger: a.Logger,
+		Store:  a.Store,
+	}
+
+	handler.Authentication{
+		JWT:    a.JWT,
+		Logger: a.Logger,
+		Store:  a.Store,
+	}.Register(app.Group("/auth"))
+
+	handler.User{
+		Store:  a.Store,
+		Logger: a.Logger,
+	}.Register(app.Group("/users", auth.Auth))
+
+	handler.Idea{
+		Store:  a.Store,
+		Logger: a.Logger,
+	}.Register(app.Group("/ideas", auth.Auth))
+
+	handler.Company{
+		Store:  a.Store,
+		Logger: a.Logger,
+	}.Register(app.Group("/companies", auth.Auth))
+
+	handler.Category{
+		Store:  a.Store,
+		Logger: a.Logger,
+	}.Register(app.Group("/categories", auth.Auth))
+
+}
