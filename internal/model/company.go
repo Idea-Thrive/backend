139bf76ab38b0a0a3a0728a301002883baa6135a
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-14 15:42:23 +0430

    change created at and updated at to string

--- internal/model/company.go
+++ internal/model/company.go
@@ -1,15 +1,13 @@
 package model
 
-import "time"
-
 // Company struct.
 type Company struct {
-	CompanyID       string    `json:"company_id,omitempty"`
-	Name            string    `json:"name,omitempty"`
-	LogoURL         string    `json:"logo_url,omitempty"`
-	OwnerNationalID string    `json:"owner_national_id,omitempty"`
-	OwnerFirstName  string    `json:"owner_first_name,omitempty"`
-	OwnerLastName   string    `json:"owner_last_name,omitempty"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
+	CompanyID       string `json:"company_id,omitempty"`
+	Name            string `json:"name,omitempty"`
+	LogoURL         string `json:"logo_url,omitempty"`
+	OwnerNationalID string `json:"owner_national_id,omitempty"`
+	OwnerFirstName  string `json:"owner_first_name,omitempty"`
+	OwnerLastName   string `json:"owner_last_name,omitempty"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
 }
