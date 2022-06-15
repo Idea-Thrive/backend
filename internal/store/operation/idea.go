667a2f2b55106dddd61590c5eb8fdcf2ac1344b5
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-15 13:46:53 +0430

    feat: create idea function added.

--- internal/store/operation/idea.go
+++ internal/store/operation/idea.go
@@ -1,19 +1,53 @@
 package operation
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/Idea-Thrive/backend/internal/model"
 )
 
-func (u *Operation) IdeaCreate(idea model.Idea) error {
-	return nil
+var (
+	errNotInsertedInIdeaTable = errors.New("not inserted in idea table")
+)
+
+// IdeaCreate function.
+func (u *Operation) IdeaCreate(idea model.Idea) (err error) {
+	fmt.Println("here")
+	queryString := "INSERT INTO `Idea`(`category`, `title`, `description`," +
+		" `up_vote`, `down_vote`, `creator_id`, `company_id`," +
+		" `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+	result, err := u.DB.Exec(queryString,
+		idea.Category,
+		idea.Title,
+		idea.Description,
+		idea.UpVoteCount,
+		idea.DownVoteCount,
+		idea.CreatorID,
+		idea.CompanyID,
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		return err //nolint:wrapcheck
+	}
+
+	lid, _ := result.LastInsertId()
+	if lid == 0 {
+		err = errNotInsertedInIdeaTable
+
+		return err
+	}
+
+	return err
 }
 
 func (u *Operation) IdeaGet(id string) (*model.Idea, error) {
 	return &model.Idea{
 		Title:         "good idea",
-		Category:      "others",
+		Category:      1,
 		Description:   "this is a very good idea",
 		UpVoteCount:   10,
 		DownVoteCount: -5,
@@ -27,7 +61,7 @@ func (u *Operation) IdeaGet(id string) (*model.Idea, error) {
 func (u *Operation) IdeaGetAll(companyID, category string, size, offset int) ([]model.Idea, error) {
 	idea := model.Idea{
 		Title:         "good idea",
-		Category:      "others",
+		Category:      1,
 		Description:   "this is a very good idea",
 		UpVoteCount:   10,
 		DownVoteCount: -5,
