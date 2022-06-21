1316e9a1281f5fce09723a854bdf28243ebd72cc
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-21 18:55:34 +0430

    fix: change created type and update type to string.

--- internal/model/Category.go
+++ internal/model/Category.go
@@ -1,11 +1,9 @@
 package model
 
-import "time"
-
 // Category struct.
 type Category struct {
-	Name        string    `json:"name,omitempty"`
-	Description string    `json:"description,omitempty"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
 }
