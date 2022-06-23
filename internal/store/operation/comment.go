ab07797c520f856ebf464201786fa040499d034d
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-24 02:54:01 +0430

    feat: add commentCreate function.

--- internal/store/operation/comment.go
+++ internal/store/operation/comment.go
@@ -1,1 +1,40 @@
 package operation
+
+import (
+	"errors"
+	"github.com/Idea-Thrive/backend/internal/model"
+	"time"
+)
+
+var (
+	errNotInsertedInCommentTable = errors.New("not inserted in comment table")
+)
+
+// CommentCreate function.
+func (u *Operation) CommentCreate(comment model.Comment) (err error) {
+	queryString := "INSERT INTO `Comment`(`user_id`, `company_id`, `idea_id`, " +
+		"`score`, `description`, `created_at`, " +
+		"`updated_at`)  VALUES (?, ?, ?, ?, ?, ?, ?)"
+
+	result, err := u.DB.Exec(queryString,
+		comment.UserID,
+		comment.CompanyID,
+		comment.IdeaID,
+		comment.Score,
+		comment.Description,
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		return err //nolint:wrapcheck
+	}
+
+	lid, _ := result.LastInsertId()
+	if lid == 0 {
+		err = errNotInsertedInCommentTable
+
+		return err
+	}
+
+	return nil
+}
--- internal/store/operation/operation.go
+++ internal/store/operation/operation.go
@@ -19,17 +19,13 @@ func NewOperation(db *sql.DB, logger *zap.Logger) *Operation {
 	return &Operation{DB: db, Logger: logger}
 }
 
-func (u *Operation) CommentCreate(comment model.Comment) error {
-	return nil
-}
-
 func (u *Operation) CommentGetAll(ideaID string, scoreOnly bool, size, offset int) ([]model.Comment, error) {
 	comment := model.Comment{
 		Score:       2,
 		Description: "yes",
-		UserID:      123123,
-		CompanyID:   2352354,
-		IdeaID:      423534543,
+		UserID:      "123123",
+		CompanyID:   "2352354",
+		IdeaID:      "423534543",
 		CreatedAt:   time.Now().String(),
 		UpdatedAt:   time.Now().String(),
 	}
