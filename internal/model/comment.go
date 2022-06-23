63e25cb194c7f80403d4d4d5ae87df4cbf6ea444
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-24 02:53:40 +0430

    change datatypes to string

--- internal/model/comment.go
+++ internal/model/comment.go
@@ -4,9 +4,9 @@ package model
 type Comment struct {
 	Score       int    `json:"score,omitempty"`
 	Description string `json:"description,omitempty"`
-	UserID      int    `json:"user_id,omitempty"`
-	CompanyID   int    `json:"company_id,omitempty"`
-	IdeaID      int    `json:"idea_id,omitempty"`
+	UserID      string `json:"user_id,omitempty"`
+	CompanyID   string `json:"company_id,omitempty"`
+	IdeaID      string `json:"idea_id,omitempty"`
 	CreatedAt   string `json:"created_at"`
 	UpdatedAt   string `json:"updated_at"`
 }
