4991bdcf331c68931d793ae86b08fe7853e21e15
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-23 21:04:23 +0430

    add company id to category model

--- internal/model/Category.go
+++ internal/model/Category.go
@@ -2,6 +2,7 @@ package model
 
 // Category struct.
 type Category struct {
+	CompanyID   string `json:"company_id"`
 	Name        string `json:"name,omitempty"`
 	Description string `json:"description,omitempty"`
 	CreatedAt   string `json:"created_at"`
