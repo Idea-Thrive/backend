fa9ce6b08607181064097047ee6310a791897ace
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-06-25 10:09:23 +0430

    feat: add get user by username

--- internal/store/operation/operation.go
+++ internal/store/operation/operation.go
@@ -2,6 +2,8 @@ package operation
 
 import (
 	"database/sql"
+
+	"github.com/Idea-Thrive/backend/internal/model"
 	"go.uber.org/zap"
 )
 
@@ -14,4 +16,18 @@ type Operation struct {
 // NewOperation function.
 func NewOperation(db *sql.DB, logger *zap.Logger) *Operation {
 	return &Operation{DB: db, Logger: logger}
+}
+
+func (o *Operation) UserGetByUsername(username string) (user model.User, err error) {
+	return model.User{
+		FirstName:   "first-name",
+		LastName:    "last-name",
+		Email:       "123@gmail.com",
+		PhoneNumber: "0985858585",
+		PhotoURL:    "",
+		CompanyID:   "1234698755",
+		PersonnelID: username,
+		Gender:      "male",
+		Role:        "employee",
+	}, nil
 }
