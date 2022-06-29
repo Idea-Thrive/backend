d7dd50696696dc9df0eebb8f3272f81f67b54dc6
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-30 02:08:36 +0430

    remove up_vote and down_vote from idea model

--- internal/model/idea.go
+++ internal/model/idea.go
@@ -2,14 +2,12 @@ package model
 
 // Idea struct.
 type Idea struct {
-	IdeaID        string `json:"idea_id,omitempty"`
-	Category      int    `json:"category,omitempty"`
-	Title         string `json:"title,omitempty"`
-	Description   string `json:"description,omitempty"`
-	UpVoteCount   int    `json:"up_vote_count,omitempty"`
-	DownVoteCount int    `json:"down_vote_count,omitempty"`
-	CreatorID     string `json:"creator_id,omitempty"`
-	CompanyID     string `json:"company_id,omitempty"`
-	CreatedAt     string `json:"created_at"`
-	UpdatedAt     string `json:"updated_at"`
+	IdeaID      string `json:"idea_id,omitempty"`
+	Category    int    `json:"category,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	CreatorID   string `json:"creator_id,omitempty"`
+	CompanyID   string `json:"company_id,omitempty"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
 }
