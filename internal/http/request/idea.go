d7079d29bbc7cd8bcca63ca3bad5886536365553
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-30 02:12:27 +0430

    up_vote and down_vote were removed from the queris and handlers

--- internal/http/request/idea.go
+++ internal/http/request/idea.go
@@ -4,8 +4,6 @@ type IdeaCreation struct {
 	Category    int    `json:"category"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
-	UpVote      int    `json:"up_vote"`
-	DownVote    int    `json:"down_vote"`
 	CreatorID   string `json:"creator_id"`
 	CompanyID   string `json:"company_id"`
 }
