0e8fe76fed055f8e84e390fe9c81e8082641ee10
Amir Hallaji <a.hallaji.b@gmail.com>
2022-06-30 21:38:38 +0430

    feat: handling user authentication.

--- internal/store/operation/authentication.go
+++ internal/store/operation/authentication.go
@@ -1,6 +1,31 @@
 package operation
 
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	errNoRecordWithThisEmail = errors.New("there is no record with this email")
+	errWrongPassword         = errors.New("wrong password entered")
+)
+
 // Login function.
 func (u *Operation) Login(email, password string) (bool, error) {
-	return true, nil
+	fmt.Println("email: ", email)
+	fmt.Println("password: ", password)
+	var realPassword string
+	errRetrieve := u.DB.QueryRow("SELECT `password` FROM `User` WHERE `email` = ?", email).Scan(
+		&realPassword,
+	)
+
+	if errRetrieve != nil {
+		return false, errNoRecordWithThisEmail
+	}
+
+	if realPassword == password {
+		return true, nil
+	}
+
+	return false, errWrongPassword
 }
