5bac778330fc6d8e22496af00810538691c1a82a
Mehdi <mehditeymorian322@gmail.com>
2022-06-30 19:00:31 +0430

    feat: run serve command as root command

--- cmd/root.go
+++ cmd/root.go
@@ -4,23 +4,21 @@ import (
 	"os"
 
 	"github.com/Idea-Thrive/backend/internal/cmd/serve"
-
-	"github.com/spf13/cobra"
 )
 
 // Execute function.
 func Execute() {
-	rootCmd := &cobra.Command{
-		Use:   "backend",
-		Short: "backend of idea-thrive service",
-		Long:  `backend of idea-thrive service`,
-	}
+	//rootCmd := &cobra.Command{
+	//	Use:   "backend",
+	//	Short: "backend of idea-thrive service",
+	//	Long:  `backend of idea-thrive service`,
+	//}
+	//
+	//rootCmd.AddCommand(
+	//	serve.Command(),
+	//)
 
-	rootCmd.AddCommand(
-		serve.Command(),
-	)
-
-	err := rootCmd.Execute() //nolint:ifshort
+	err := serve.Command().Execute() //nolint:ifshort
 	if err != nil {
 		os.Exit(1)
 	}
