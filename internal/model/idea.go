fc7dfd4a2cd1ddc65a4dfd40ade75ee2d49f6d7a
Amir Hallaji <a.hallaji.b@gmail.com>
2022-07-01 21:14:07 +0430

    feat: add category color to idea model

--- internal/model/idea.go
+++ internal/model/idea.go
@@ -2,13 +2,14 @@ package model
 
 // Idea struct.
 type Idea struct {
-	ID           string `json:"id,omitempty"`
-	CategoryID   string `json:"category_id,omitempty"`
-	CategoryName string `json:"category_name"`
-	Title        string `json:"title,omitempty"`
-	Description  string `json:"description,omitempty"`
-	CreatorID    string `json:"creator_id,omitempty"`
-	CompanyID    string `json:"company_id,omitempty"`
-	CreatedAt    string `json:"created_at"`
-	UpdatedAt    string `json:"updated_at"`
+	ID            string `json:"id,omitempty"`
+	CategoryID    string `json:"category_id,omitempty"`
+	CategoryName  string `json:"category_name"`
+	CategoryColor string `json:"category_color"`
+	Title         string `json:"title,omitempty"`
+	Description   string `json:"description,omitempty"`
+	CreatorID     string `json:"creator_id,omitempty"`
+	CompanyID     string `json:"company_id,omitempty"`
+	CreatedAt     string `json:"created_at"`
+	UpdatedAt     string `json:"updated_at"`
 }
