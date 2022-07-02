6e4119248ab51ebeb44574acf2c379045de116f0
Amir Hallaji <a.hallaji.b@gmail.com>
2022-07-03 03:40:58 +0430

    fix linter errors for cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -8,16 +8,6 @@ import (
 
 // Execute function.
 func Execute() {
-	//rootCmd := &cobra.Command{
-	//	Use:   "backend",
-	//	Short: "backend of idea-thrive service",
-	//	Long:  `backend of idea-thrive service`,
-	//}
-	//
-	//rootCmd.AddCommand(
-	//	serve.Command(),
-	//)
-
 	err := serve.Command().Execute()
 	if err != nil {
 		os.Exit(1)
