6abf40d189b3d7fe49acb4da02dd8fecf4a35f06
Amir Hallaji <a.hallaji.b@gmail.com>
2022-07-03 02:33:54 +0430

    feat: add is_approved item to the get query for IdeaGet API

--- internal/store/operation/idea.go
+++ internal/store/operation/idea.go
@@ -44,11 +44,12 @@ func (u *Operation) IdeaCreate(idea model.Idea) (err error) {
 // IdeaGet function.
 func (u *Operation) IdeaGet(id string) (idea model.Idea, err error) {
 
-	errRetrieve := u.DB.QueryRow("SELECT `category_id`, `title`, `description`,"+
+	errRetrieve := u.DB.QueryRow("SELECT `category_id`, `title`, `description`, `is_approved`"+
 		" `creator_id`, `company_id`, `created_at`, `updated_at` FROM `Idea` WHERE `id` = ?", id).Scan(
 		&idea.CategoryID,
 		&idea.Title,
 		&idea.Description,
+		&idea.IsApproved,
 		&idea.CreatorID,
 		&idea.CompanyID,
 		&idea.CreatedAt,
